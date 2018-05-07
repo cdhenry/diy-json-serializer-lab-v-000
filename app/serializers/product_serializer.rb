63bb4ed4c230ee93f338e185d42619619311da82
Chris Henry <chrishenry@protonmail.com>
2018-05-07 20:56:46 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,7 +6,11 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
     serialized_product += '"price": ' + product.price.to_s + ', '
+<<<<<<< HEAD
     serialized_product += '"inventory": ' + product.inventory.to_s + ' '
+=======
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+>>>>>>> 4acde0c737996575f8fbe7f18e4d596628810bab
 
     serialized_product += '}'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+<<<<<<< HEAD
   resources :products, only: [:show, :index, :new, :create]
+=======
+  resources :products, only: [:index, :new, :create, :show]
+>>>>>>> 4acde0c737996575f8fbe7f18e4d596628810bab
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/data', to: 'products#data'
